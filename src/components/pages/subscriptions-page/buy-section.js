700a15f96c281883f0d33be2ba9f3e4c5066030f
Arsen <ars.toktarov@gmail.com>
2021-09-25 16:16:13 +0600

    Production

---
+++ src/components/pages/subscriptions-page/buy-section.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Box} from "@material-ui/core";
+
+const BuySection = () => {
+
+    return (
+        <Box>
+
+        </Box>
+    );
+
+}
+
+export default BuySection;
